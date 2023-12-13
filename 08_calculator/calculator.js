ddc5b874700a668059dc4b2b987e8e7c1a47298f
Ali <89438079+AliBa1@users.noreply.github.com>
2023-12-12 23:41:40 -0800

    Finish 09 palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sum = 0;
+  nums.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let ans = 1;
+  nums.forEach(num => {
+    ans *= num;
+  });
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let ans = 1;
+  for (let i=0; i<b; i++) {
+    ans *= a;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let ans = 1;
+  for (let i=0; i<a; i++) {
+    ans *= i+1;
+  }
+  return ans;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    /*
+    function isLetter(char) {
+        return /^[a-zA-Z]$/.test(char);
+    }
 
+    let reversedWord = '';
+    let symbols = '';
+    // string.toLowerCase(); 
+    for (let i=string.length-1; i>-1; i--) {
+        if (isLetter(string[i])){
+            reversedWord += string[i];
+        } else {
+            symbols += string[i];
+        }
+    }
+
+    reversedWord += symbols;
+    */
+
+    let reversedWord = string.split('').reverse().filter(char => /^[a-zA-Z0-9]$/.test(char)).join('');
+    string = string.split('').filter(char => /^[a-zA-Z0-9]$/.test(char)).join('');
+
+    if (reversedWord.toLowerCase() == string.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
