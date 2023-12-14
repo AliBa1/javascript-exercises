0847f35bfcd54cc8d925175160ed2a9b3ca658eb
Ali <89438079+AliBa1@users.noreply.github.com>
2023-12-13 22:36:40 -0800

    Complete all

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num<0) {
+        return "OOPS";
+    }
+    let i=2;
+    let sum = [1, 1];
+    while (i<num) {
+        sum[i] = sum[i-1] + sum[i-2];
+        i++;
+    }
 
+    return sum[num-1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = [];
+    let age = 0;
+    let oldestIndex = 0;
+    let oldestAge = 0;
 
+    for (let i = 0; i<people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            age = 2023 - people[i].yearOfBirth;
+        } else {
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+
+        if(age>oldestAge) {
+            oldestIndex = i;
+            oldestAge = age;
+        }
+    }
+    
+    return people[oldestIndex];
 };
 
 // Do not edit below this line
